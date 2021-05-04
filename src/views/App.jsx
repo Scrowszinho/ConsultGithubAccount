1d460fc9407669622600ec37e3459f528dd3b22f
Luis Gustavo <gustascrows@gmail.com>
2021-05-04 16:02:16 -0300

    View: Index & app

---
+++ src/views/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import SearchTop from './layouts/seatchTop/SearchTop';
+import UserInfos from './layouts/userInfos/UserInfos';
+const App = props => {
+    return (
+        <div className='App'>
+            <SearchTop />
+        </div>
+    )
+}
+
+export default App
